3050ebb009edee9df55ec813af345cd18f8e9dab
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2020-01-13 17:56:07 +0300

    Did refactoring: added indentation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,33 +14,33 @@ router.post(
             .isLength({ min: 6 })
     ],
     async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                massege: 'Некорректные данные при регистрации'
-            })
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    massege: 'Некорректные данные при регистрации'
+                })
+            }
+
+            const { email, password } = req.body;
+            const candidate = await User.findOne({ email });
+
+            if(candidate) {
+                return res.status(400)
+                    .json('Пользователь с таким email уже существует!');
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email, password: hashedPassword });
+
+            await user.save();
+            res.status(201).json({ message: 'Пользователь создан' });
+        } catch(e) {
+            res.status(500)
+                .json('Что-то пошло не так! Попробуйте снова.');
+            console.log('Server Error: ', e.message);
         }
-
-        const { email, password } = req.body;
-        const candidate = await User.findOne({ email });
-
-        if(candidate) {
-            return res.status(400)
-                .json('Пользователь с таким email уже существует!');
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = new User({ email, password: hashedPassword });
-
-        await user.save();
-        res.status(201).json({ message: 'Пользователь создан' });
-    } catch(e) {
-        res.status(500)
-            .json('Что-то пошло не так! Попробуйте снова.');
-        console.log('Server Error: ', e.message);
-    }
 });
 
 //api/auth/login
