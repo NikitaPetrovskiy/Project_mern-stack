fa4ccc904b24f2b6c6b8617f582a6ad98b9c545c
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2020-01-14 12:20:25 +0300

    Created routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import 'materialize-css';
 
+import { useRouter } from './routes';
+
 function App() {
+  const routes = useRouter(false);
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="container">
+        { routes }
+      </div>
+    </Router>
   );
 }
 
